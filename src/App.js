f674a01464a9238aa5e92f4994576213cebb8b3b
Prasun117 <57390215+Prasun117@users.noreply.github.com>
2021-12-10 18:27:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./styles.css";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow
-} from "@material-ui/core";
 
 import { FilterList } from "./Components/FilterList";
 import { TableComponent } from "./Components/TableComponent";
-import { FilterComponent } from "./Components/TableFilter";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <FilterList />
       <TableComponent />
     </div>
